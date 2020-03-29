854657adb76f20b50ca0d30752cd893f9b6a820e
Francisco Claude-Faust <fclaude@recoded.cl>
2020-03-28 21:37:17 -0700

    Adjusted max length of name

--- main.go
+++ main.go
@@ -112,7 +112,7 @@ const page = `
 			<form action="/generate/" method="POST">
 			  <div class="form-group">
 				<label for="name">Child Name (Optional):</label>
-				<input type="text" name="name" class="form-control" id="name" maxlength="10">
+				<input type="text" name="name" class="form-control" id="name" maxlength="20">
 			  </div>
 			  <button type="submit" class="btn btn-default btn-primary">Generate</button>
 			</form> 
