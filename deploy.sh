59f21c76bac49784b335d7ada3ea0b2db119b14e
removedporn <86824510+removedporn@users.noreply.github.com>
2022-06-20 20:49:54 +0800

    Delete deploy.sh

--- deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-go build .
-
-scp dicemath fclaude@dicemath.recoded.cl:/home/fclaude/dicemath/dicemath.new
-ssh fclaude@dicemath.recoded.cl "mv /home/fclaude/dicemath/dicemath /home/fclaude/dicemath/dicemath.old"
-ssh fclaude@dicemath.recoded.cl "mv /home/fclaude/dicemath/dicemath.new /home/fclaude/dicemath/dicemath"
-ssh fclaude@dicemath.recoded.cl "sudo service dicemath restart"
